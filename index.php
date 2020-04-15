016a97b42415edae603db74d649ec5e688542db4
BlackPeace5 <57791853+BlackPeace5@users.noreply.github.com>
2020-04-15 16:56:06 +0300

    11

--- index.php
+++ index.php
@@ -1,33 +1,33 @@
 <?php
 define("BASEURI", __DIR__);
-ini_set("display_errors", 1); error_reporting (-1); 
+ini_set("display_errors", 1); error_reporting (-1);
+
 use gazetov\GazetovException;
 use gazetov\MyLog;
-use gazetov\Linear;
 use gazetov\Square;
+require_once('vendor/autoload.php');
 
-
-include BASEURI . '/core/EquationInterface.php';
-include BASEURI . '/core/LogInterface.php';
-include BASEURI . '/core/LogAbstract.php';
-include BASEURI . '/gazetov/GazetovException.php';
-include BASEURI . '/gazetov/MyLog.php';
-include BASEURI . '/gazetov/Linear.php';
-include BASEURI . '/gazetov/Square.php';
+include BASEURI . "/vendor/rok9ru/trpo-core/EquationInterface.php";
+include BASEURI . "/vendor/rok9ru/trpo-core/LogInterface.php";
+include BASEURI . "/vendor/rok9ru/trpo-core/LogAbstract.php";
+include BASEURI . "/vendor/blackpeace5/lab2/GazetovException.php";
+include BASEURI . "/vendor/blackpeace5/lab2/MyLog.php";
+include BASEURI . "/vendor/blackpeace5/lab2/Linear.php";
+include BASEURI . "/vendor/blackpeace5/lab2/Square.php";
 
 	try{
 		echo 'Enter a, b, c' . "\n";
 		$num = [];
 		for($i = 0; $i < 3; $i++){
-		$num[$i] = readline("Value = ");	
+		$num[$i] = readline("Value = ");
 		}
 		if($num[0] != 0){
 		MyLog::log("Vvedeno uravneniye: " . $num[0] . "x^2 + " . $num[1] . "x + " . $num[2] . " = 0");
 		} else {
-		MyLog::log("Vvedeno uravneniye: " . $num[0] . "x + " . $num[1] . " = 0");
+		MyLog::log("Vvedeno uravneniye: " . $num[1] . "x + " . $num[2] . " = 0");
 		}
 
-		$square = new Square(null);
+		$square = new  Square(null);
 		$roots = $square->solve($num[0], $num[1], $num[2]);
 
 		if ($roots != false) {
---
+++ tests/SquareTest.php
@@ -0,0 +1,62 @@
+<?php
+namespace gazetov;
+
+require_once __DIR__.'/../core/EquationInterface.php';
+require_once __DIR__.'./../core/LogAbstract.php';
+require_once __DIR__.'./../core/LogInterface.php';
+require_once __DIR__.'./../Gazetov/GazetovException.php';
+require_once __DIR__.'./../Gazetov/MyLog.php';
+require_once __DIR__.'./../Gazetov/Linear.php';
+require_once __DIR__.'./../Gazetov/Square.php';
+
+use PHPUnit\Framework\TestCase;
+
+Class SquareTest extends TestCase
+{
+  protected $square;
+
+  protected function setUp() : void
+  {
+    $this->square= new Square();
+  }
+
+  /**
+  * @dataProvider providerSquareEquation
+  *  Проверка решения квадратного уравнения
+  */
+
+  public function testSquareEquation($a, $b, $c, $result) : void
+  {
+    $this->assertEquals($result, $this->square->solve($a, $b, $c));
+  }
+
+  public function providerSquareEquation() : array
+  {
+    return array(
+      array(-3, 0, 75, [-5,5]),
+      array(4, -12, 9, [1.5]),
+      array(0, 2, -8, 4)
+    );
+  }
+
+  /**
+  * @dataProvider providerException
+  *  Проверка выбрасывания ошибки
+  */
+
+  public function testException($a, $b, $c) : void
+  {
+    $this->expectException(GazetovException::Class);
+    $this->square->solve($a, $b, $c);
+  }
+
+  public function providerException() : array
+  {
+    return array(
+      array(0, 0, 0),
+      array(-4, 0, -20),
+      array(0, "b", -9),
+    );
+  }
+}
+?>
